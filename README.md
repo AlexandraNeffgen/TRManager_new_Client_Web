4a20062d210d4c9fcf92b6e8987a2f04fad41954
Alexandra Neffgen <a.neffgen@me.com>
2016-01-11 21:49:11 +0100

    erstes Erstellen

--- README.md
+++
@@ -1,2 +0,0 @@
-# TRManager_new_Client_Web
-Web Client für den TR_Manager
---
+++ TRManager_new_Client_Web/src/TRManager_new_Client_Web/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TRManager_new_Client_Web.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
